187ab927aa895af64aeddb5b9ad33afb980fbf01
Carolina Perpetuo <carolinaperpetuocosta@gmail.com>
2026-05-25 09:34:40 -0300

    blog pessoal

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,73 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+// anotações serve para alterar ou definir comportamentos
+
+@Entity // definido que a classe Postagem vai se tornar uma tabela
+@Table(name = "tb_postagens") // definir nome da tabela seguindo a nossa nomeclatura
+
+public class Postagem {
+
+	@Id // define a chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // definir como é preenchido pelo banco de dados
+	private Long id;  //o long aqui vira o bigint no sql
+		
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo título deve ter no minimo 5 e no máximo 100 caracteres.")
+	private String titulo;
+	
+	//titulo varchar(100) NOT NULL \ "titulo"
+	
+	@NotBlank(message = "O atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve ter no minimo 10 e no máximo 1000 caracteres.")
+	private String texto;
+	
+	// texto varchar(1000) NOT NULL \ "texto..."
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+}
